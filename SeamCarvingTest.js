66a02b166a9bbce765da97fb3005582eac708af3
Eric LaForce <elaforc@gmail.com>
2019-11-08 16:26:00 -0500

    Computed back pointers table

--- SeamCarvingTest.js
+++ SeamCarvingTest.js
@@ -1,6 +1,51 @@
+let SeamCarvingBackPointer = class {
+  constructor(energy, pointer, current) {
+    this.energy = energy;
+    this.current = current;
+    this.xPointer = pointer;
+  }
+}
+
 let SeamCarving = class {
   constructor(arr) {
     this.energies = arr;
+  }
+
+  findMinimumParent(data) {
+    let current = data[0];
+    for (let i = 1; i < data.length; i++) {
+      if (data[i].energy < current.energy) {
+        current = data[i];
+      }
+    }
+    return current;
+  }
+
+  computeMinimumSeam() {
+    let seamEnergies = [];
+    let previousSeam = [];
+    for(let i = 0; i < this.energies[0].length; i++) {
+      previousSeam.push(new SeamCarvingBackPointer(this.energies[0][i], null, i));
+    }
+    seamEnergies.push(previousSeam);
+
+    for (let i = 1; i < this.energies.length; i++) {
+      let energiesRow = this.energies[i];
+
+      let seamEnergiesRow = [];
+      for (let j = 0; j < energiesRow.length; j++) {
+        const left = Math.max(j - 1, 0);
+        const right = Math.min(j + 1, energiesRow.length - 1);
+        const minParent = this.findMinimumParent(seamEnergies[i-1].slice(left, right + 1));
+        const minSeamEnergy = new SeamCarvingBackPointer(energiesRow[j] + seamEnergies[i-1][minParent.current].energy, 
+                                                          minParent.current, j);
+        seamEnergiesRow.push(minSeamEnergy);
+      }
+
+      seamEnergies.push(seamEnergiesRow);
+    }
+
+    return seamEnergies;
   }
 
   computeMinimumSeamEnergy() {
@@ -31,5 +76,5 @@ const arr = [
 ];
 
 const seamCarving = new SeamCarving(arr);
-console.log(seamCarving.computeMinimumSeamEnergy());
+console.log(seamCarving.computeMinimumSeam());
 
